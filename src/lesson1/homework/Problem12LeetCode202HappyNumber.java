1c4fed84791282723032b9c9f7451af1e4a3578d
thanh.tran <thanh.tran@evizi.com>
2021-10-07 07:12:58 +0700

    update isHappyNumber and implement the subarrays counter

--- src/lesson1/homework/Problem12LeetCode202HappyNumber.java
+++ src/lesson1/homework/Problem12LeetCode202HappyNumber.java
@@ -6,7 +6,8 @@ import java.util.Set;
 public class Problem12LeetCode202HappyNumber {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean isHappy = solution.isHappy(1111111);
+        //boolean isHappy = solution.isHappyV2(1111111);
+        boolean isHappy = solution.isHappyV2(19);
     }
 
     private static class Solution {
@@ -30,13 +31,27 @@ public class Problem12LeetCode202HappyNumber {
 
         private int sumOfSquaresOfDigits(int n) {
             int sum = 0;
-            int rest = 0;
+            int remainder  = 0;
             while (n > 0) {
-                rest = n % 10;
-                sum += rest * rest;
+                remainder  = n % 10;
+                sum += remainder  * remainder;
                 n = n / 10;
             }
             return sum;
+        }
+
+        //use Floyd Algorithm - fast and slow pointer
+        public boolean isHappyV2(int n) {
+            int fastSum = n;
+            int slowSum = n;
+            do {
+                slowSum = sumOfSquaresOfDigits(slowSum);
+                fastSum = sumOfSquaresOfDigits(sumOfSquaresOfDigits(fastSum));
+                if(slowSum == 1 || fastSum == 1) {
+                    return true;
+                }
+            } while (fastSum != slowSum);  //it contains a cycle - loops endlessly in a cycle
+            return false;
         }
     }
 }
